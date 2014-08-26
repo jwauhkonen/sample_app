42cef54f2b1911db47ed0cc764f468ed19b70854
Jonathan Wauhkonen <jwauhkonen@gmail.com>
2014-08-26 11:12:59 -0400

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
   #get "foo_bars/baz" - I thought I deleted this.
   #get "foo_bars/quuz" - This one too. . .
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  root 'static_pages#home'
+  match '/signup',  to: 'users#new',             via: 'get'
+  match '/help',    to: 'static_pages#help',     via: 'get'
+  match '/about',   to: 'static_pages#about',    via: 'get'
+  match '/contact', to: 'static_pages#contact',  via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
